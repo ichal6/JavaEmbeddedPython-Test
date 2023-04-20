e6751c6d195d9f8fb93bd39dd61f638ca9cfd2bc
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-04-20 15:03:19 +0200

    Refactor method

--- src/main/java/pl/lechowicz/Main.java
+++ src/main/java/pl/lechowicz/Main.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         String pythonFolder = System.getenv("LD_LIBRARY_PATH");
-        System.out.println(pythonFolder);
         String jepPath = pythonFolder + "/jep/libjep.jnilib";
         if (!Files.exists(Path.of(jepPath))){
             jepPath = pythonFolder + "/jep/libjep.so";
@@ -27,7 +26,7 @@ public class Main {
         // run each function from the .py doc I
         subInterp.eval("import jep_path as p");
         subInterp.eval("res_spacy = p.run_spacy_nlp('Apple is looking at buying U.K. startup for $1 billion')");
-        ArrayList result = (ArrayList) subInterp.getValue("res_spacy");
+        ArrayList result = subInterp.getValue("res_spacy", ArrayList.class);
         for (Object item:result) {
             System.out.println(item.toString());
         }
